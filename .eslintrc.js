a3eee3d4227d25c37022d6d56c164fa4ea4dc814
subdiox <subdiox@gmail.com>
2023-02-19 08:31:48 +0900

    Remove fiahfy eslint dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,22 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['@fiahfy/vue'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/recommended',
+        'prettier',
+      ],
+    },
+  ],
 }
